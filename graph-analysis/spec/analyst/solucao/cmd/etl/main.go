c1d6a76d9586f26218d0c601a345fe0f94b1a990
Faracoeng <alfmazucheli@gmail.com>
2025-04-27 18:41:55 -0300

    arquitetura hexagonal implementada na API

--- graph-analysis/spec/analyst/solucao/cmd/etl/main.go
+++ graph-analysis/spec/analyst/solucao/cmd/etl/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/Faracoeng/jobs/graph-analysis/spec/analyst/solucao/internal/config"
 	"github.com/Faracoeng/jobs/graph-analysis/spec/analyst/solucao/internal/loader"
 	"github.com/Faracoeng/jobs/graph-analysis/spec/analyst/solucao/internal/reader"
-	repo "github.com/Faracoeng/jobs/graph-analysis/spec/analyst/solucao/internal/infra/db"
+	repo "github.com/Faracoeng/jobs/graph-analysis/spec/analyst/solucao/internal/infra/db/neo4j"
 )
 
 func main() {
